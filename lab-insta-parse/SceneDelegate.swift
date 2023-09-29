fcd3561cfa16d53b304f760c1272d20570767d9c
Kevin Horta <100811318+vzkevin@users.noreply.github.com>
2023-09-29 15:37:11 -0400

    final commit

--- lab-insta-parse/SceneDelegate.swift
+++ lab-insta-parse/SceneDelegate.swift
@@ -33,6 +33,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         }
 
         // TODO: Pt 1 - Check for cached user for persisted log in.
+        // Check if a current user exists
+        if User.current != nil {
+            login()
+        }
+
 
     }
 
